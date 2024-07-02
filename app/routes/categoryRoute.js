10b09e7c40a45c6fd0df893b64fb717437ede29e
sovisrushain <sovisrushain@gmail.com>
2024-07-03 01:00:42 +0530

    delete product & category

--- app/routes/categoryRoute.js
+++ app/routes/categoryRoute.js
@@ -4,5 +4,6 @@ const categoryController = require("../controller/categoryController");
 router.get('/categories', categoryController.getAllCategories)
 router.post('/categories', categoryController.createCategory)
 router.put('/categories/:id', categoryController.updateCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 
 module.exports = router
--- app/routes/productRoute.js
+++ app/routes/productRoute.js
@@ -4,5 +4,6 @@ const productController = require("../controller/productController");
 router.get('/products', productController.getAllProducts)
 router.post('/products', productController.createProduct)
 router.put('/products/:id', productController.updateProduct)
+router.delete('/products/:id', productController.deleteProduct)
 
 module.exports = router
