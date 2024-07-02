dbf3cc6ced7512afe4b3a7bf0adfdbff409299ec
sovisrushain <sovisrushain@gmail.com>
2024-07-02 22:16:38 +0530

    set up db connection

---
+++ app/services/database.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'cisco',
+    database: 'crud',
+    port: 5432,
+})
+
+module.exports = pool
